66ccc232a5e55c7936e43f67b9a67ac6065a5569
Kabin Dhital <kabin.dhital@cogenthealt.com.np>
2022-04-04 08:38:26 +0545

    added home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/nav-bar/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
-
+  @ViewChild('myTag', {static: true}) myDivTag: ElementRef | undefined;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showMenu() {    
+    const element: any = document.getElementById('navBarMobileView');
+    if (document.querySelector('.collapse')) {
+    element.classList.remove('collapse');
+    } else {
+    element.classList.add('collapse');
+    }
   }
 
 }
